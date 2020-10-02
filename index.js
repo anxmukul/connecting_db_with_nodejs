6329ca9b69d00ab20b671db228874a46a2fbab07
Mukul <kmukul1197@gmail.com>
2020-10-02 11:14:22 +0530

    Connecting Database

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('Lets Start');
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "amelia053"
+});
+con.connect(function(err){
+    if(err){
+        console.log("Error occured in connection");
+    }
+    else{
+        console.log("Connected");
+    }
+});
